e63a8d7c9f155a89dc9f3171aaa40ddb55a56236
mvitom <130609670+mvitom@users.noreply.github.com>
2023-05-03 11:42:35 +0200

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -17,32 +17,43 @@ class Channel(customtkinter.CTkFrame):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.set_label = customtkinter.CTkLabel(self,text="Channel1:",font=fontname)
-        self.set_label.grid(row=0,column=0,sticky="nsew")
         #voltage
         self.volt_label = customtkinter.CTkLabel(self,text="Voltage:",font=fontname)
         self.volt_label.grid(row=1,column=0,sticky="w")
         
         self.set_volt_e = customtkinter.CTkEntry(self,placeholder_text="(min-0.01)")
         self.set_volt_e.grid(row=1,column=1,sticky="",padx=0,pady=5)
+
+        self.get_volt_button = customtkinter.CTkButton(self,text="GET",width=60,height=10)
+        self.get_volt_button.grid(row=1,column=2,padx=5)
+        
         #current
         self.cur_label = customtkinter.CTkLabel(self,text="Current:",font=fontname)
         self.cur_label.grid(row=2,column=0,sticky="w")
         
         self.set_cur_e = customtkinter.CTkEntry(self,placeholder_text="(min-)")
         self.set_cur_e.grid(row=2,column=1,sticky="",padx=0,pady=5)
+
+        self.get_curr_button = customtkinter.CTkButton(self,text="GET",width=60,height=10)
+        self.get_curr_button.grid(row=2,column=2,padx=5)
         #limitV
         self.limV_label = customtkinter.CTkLabel(self,text="V-Limit:",font=fontname)
         self.limV_label.grid(row=3,column=0,sticky="w")
         
         self.set_limV_e = customtkinter.CTkEntry(self,placeholder_text="(min-)")
         self.set_limV_e.grid(row=3,column=1,sticky="",padx=0,pady=5)
+
+        self.get_limitV_button = customtkinter.CTkButton(self,text="GET",width=60,height=10)
+        self.get_limitV_button.grid(row=3,column=2,padx=5)
         #limitC
         self.limC_label = customtkinter.CTkLabel(self,text="C-Limit:",font=fontname)
         self.limC_label.grid(row=4,column=0,sticky="w")
         
         self.set_limC_e = customtkinter.CTkEntry(self,placeholder_text="(min-)")
         self.set_limC_e.grid(row=4,column=1,sticky="",padx=0,pady=5)
+
+        self.get_limitC_button = customtkinter.CTkButton(self,text="GET",width=60,height=10)
+        self.get_limitC_button.grid(row=4,column=2,padx=5)
 
 
 
@@ -55,35 +66,69 @@ class App(customtkinter.CTk):
         self.output_label = customtkinter.CTkLabel(self,text="Output:",font=fontname)
         self.output_label.grid(row=0,column=0)
         self.textbox = customtkinter.CTkTextbox(self,height=120,width=180)
-        self.textbox.grid(row=1, column=0,columnspan=2, padx=5, pady=5,sticky="nsew")
+        self.textbox.grid(row=1, column=0,columnspan=1, padx=5, pady=5,sticky="nsew")
         self.tcp_connect_button = customtkinter.CTkButton(self,text="connect",command=tcp_client.socket_connect,fg_color="blue",font=fontname)
-        self.tcp_connect_button.grid(row=0,column=2,pady=(10,0))
+        self.tcp_connect_button.grid(row=1,column=1,pady=(0,35))
         self.tcp_connect_button = customtkinter.CTkButton(self,text="disconnect",command=tcp_client.socket_disconnect,fg_color="red",font=fontname)
-        self.tcp_connect_button.grid(row=1,column=2)
+        self.tcp_connect_button.grid(row=1,column=1,pady=(35,0))
         #__________ch1
+        self.channel_label1 = customtkinter.CTkLabel(self,text="Channel1:",font=fontname)
+        self.channel_label1.grid(row=2,column=0,sticky="nsew")
         self.frame_ch1 = Channel(self,width=80,height=150)
-        self.frame_ch1.grid(row=2,column=0,padx=10,pady=10)
-        self.set_button_ch1 = customtkinter.CTkButton(self,text="SET",width=100,fg_color="red",command=self.set_ch1)
-        self.set_button_ch1.grid(row=3,column=0,sticky="w",padx=10)
-        self.get_button_ch1 = customtkinter.CTkButton(self,text="GET",width=100,command=self.get_ch1)
-        self.get_button_ch1.grid(row=3,column=0,padx=30,pady=5,sticky="e")
+        self.frame_ch1.grid(row=3,column=0,padx=10,pady=10)
+        self.set_button_ch1 = customtkinter.CTkButton(self,text="SET",width=80,fg_color="blue",command=self.set_ch1)
+        self.set_button_ch1.grid(row=4,column=0,sticky="w",padx=10)
+        self.switch_button_ch3 = customtkinter.CTkButton(self,text="ON",width=60,command=self.switch_ch1)
+        self.switch_button_ch3.grid(row=4,column=0)
+        self.switch_off_button_ch1 = customtkinter.CTkButton(self,text="OFF",width=60,fg_color="red")
+        self.switch_off_button_ch1.grid(row=4,column=0,sticky="e",padx=(0,35))
+
         #_____________ch2
+        self.channel_label2 = customtkinter.CTkLabel(self,text="Channel2:",font=fontname)
+        self.channel_label2.grid(row=2,column=1,sticky="nsew")
         self.frame_ch2 = Channel(self,width=80,height=150)
-        self.frame_ch2.grid(row=2,column=1,padx=10,pady=10)
-        self.set_button_ch2 = customtkinter.CTkButton(self,text="SET",width=100,fg_color="red",command=self.set_ch2)
-        self.set_button_ch2.grid(row=3,column=1,sticky="w",padx=10)
-        self.get_button_ch2 = customtkinter.CTkButton(self,text="GET",command=self.get_ch2,width=100)
-        self.get_button_ch2.grid(row=3,column=1,padx=30,pady=5,sticky="e")
+        self.frame_ch2.grid(row=3,column=1,padx=10,pady=10)
+        self.set_button_ch2 = customtkinter.CTkButton(self,text="SET",width=80,fg_color="blue",command=self.set_ch2)
+        self.set_button_ch2.grid(row=4,column=1,sticky="w",padx=10)
+        self.switch_button_ch3 = customtkinter.CTkButton(self,text="ON",width=60,command=self.switch_ch2)
+        self.switch_button_ch3.grid(row=4,column=1)
+        self.switch_off_button_ch1 = customtkinter.CTkButton(self,text="OFF",width=60,fg_color="red")
+        self.switch_off_button_ch1.grid(row=4,column=1,sticky="e",padx=(0,35))
+
         #ch3
+        self.channel_label3 = customtkinter.CTkLabel(self,text="Channel3:",font=fontname)
+        self.channel_label3.grid(row=2,column=2,sticky="nsew")
         self.frame_ch3 = Channel(self,width=80,height=150)
-        self.frame_ch3.grid(row=2,column=2,padx=10,pady=10)
-        self.set_button_ch3 = customtkinter.CTkButton(self,text="SET",width=100,fg_color="red",command=self.set_ch3)
-        self.set_button_ch3.grid(row=3,column=2,sticky="w",padx=10)
-        self.get_button_ch3 = customtkinter.CTkButton(self,text="GET",command=self.get_ch3,width=100)
-        self.get_button_ch3.grid(row=3,column=2,padx=30,pady=5,sticky="e")
-        # self.scrbar = customtkinter.CTkComboBox(self,values=["channel1", "channel2","channel3"])
-        # self.scrbar.grid(row=2,column=1)
-    
+        self.frame_ch3.grid(row=3,column=2,padx=10,pady=10)
+        self.set_button_ch3 = customtkinter.CTkButton(self,text="SET",width=80,fg_color="blue",command=self.set_ch3)
+        self.set_button_ch3.grid(row=4,column=2,sticky="w",padx=10)
+        self.switch_button_ch3 = customtkinter.CTkButton(self,text="ON",width=60,command=self.switch_ch3)
+        self.switch_button_ch3.grid(row=4,column=2)
+        self.switch_off_button_ch1 = customtkinter.CTkButton(self,text="OFF",width=60,fg_color="red")
+        self.switch_off_button_ch1.grid(row=4,column=2,sticky="e",padx=(0,35))
+
+    def switch_ch1(self):
+        state = command.switch_state()
+        message = command.switch_on_channel1()
+        try:
+            tcp_client.client_socket.send(state.encode())
+        except:
+            print("setch1: failed to send message")
+    def switch_ch2(self):
+        message = command.switch_on_channel2()
+        try:
+            tcp_client.client_socket.send(message.encode())
+        except:
+            print("setch1: failed to send message")
+    def switch_ch3(self):
+        message = command.switch_on_channel3()
+        try:
+            tcp_client.client_socket.send(message.encode())
+        except:
+            print("setch1: failed to send message")
+    def switch_off_ch1(self):
+        return
+
     def set_ch1(self):
         channel = "1"#tohle bude promenna
         volt = self.frame_ch1.set_volt_e.get()
@@ -121,12 +166,10 @@ class App(customtkinter.CTk):
             print("setch3: failed to send message")
 
 
-
 #######################################################
     def get_ch1(self):
         channel = "1"
-        message = command.get_channel(channel)
-        print(message)
+        message = command.get_voltage(channel)
         try:
             tcp_client.client_socket.send(message.encode())
         except:
@@ -139,10 +182,30 @@ class App(customtkinter.CTk):
 
     def get_ch2(self):
         channel = "2"
-        command.get_channel(channel)
+        message = command.get_channel(channel)
+        print(message)
+        try:
+            tcp_client.client_socket.send(message.encode())
+        except:
+            print("getch2: failed to send message")
+        try:
+            data = tcp_client.client_socket.recv(1024).decode()
+            print(data)
+        except:
+            print("getch2: failed to recieve data")
     def get_ch3(self):
         channel = "3"
-        command.get_channel(channel)
+        message = command.get_channel(channel)
+        print(message)
+        try:
+            tcp_client.client_socket.send(message.encode())
+        except:
+            print("getch3: failed to send message")
+        try:
+            data = tcp_client.client_socket.recv(1024).decode()
+            print(data)
+        except:
+            print("getch3: failed to recieve data")
     
 
 if __name__ == "__main__":
--- command.py
+++ command.py
@@ -1,3 +1,5 @@
+
+
 def set_channel(channel,voltage,current,limitV,limitC):
     channel = f"INST CH{channel}"
     voltage = f";VOLT {voltage}"
@@ -13,6 +15,17 @@ def get_channel(channel):
     limitV =  f";VOLT:LIM?"
     limitC =  f";CURR:LIM?"
     return channel + voltage + current + limitV + limitC
+def switch_state():
+    return "CHAN:OUTP:ALL?"
+def switch_on_channel1():
+    return "CHAN:OUTP:ALL 1,0,0"
+def switch_on_channel2():
+    return "CHAN:OUTP:ALL 0,1,0"
+def switch_on_channel3():
+    return "CHAN:OUTP:ALL 0,0,1"
+
+def switch_off_channel():
+    return
 
 # def get_channel(channel,voltage,current,limitV,limitC):
 #     get_voltage(channel,voltage)
@@ -47,23 +60,35 @@ def get_channel(channel):
 
 
 
-# def get_voltage(channel):
-#     channel = f"INST CH{channel}"
-#     voltage = f";VOLT"
-#     return channel + voltage
+def get_voltage(channel):
+    channel = f"INST CH{channel}"
+    voltage = f";VOLT"
+    return channel + voltage
 
-# def get_current(channel):
-#     channel = f"INST CH{channel}" 
-#     current = f";CURR?"
-#     return channel + current
+def get_current(channel):
+    channel = f"INST CH{channel}" 
+    current = f";CURR?"
+    return channel + current
 
-# def get_limitV(channel):
-#     channel = f"INST CH{channel}"
-#     limitV = f";VOLT:LIM?"
-#     return channel + limitV
+def get_limitV(channel):
+    channel = f"INST CH{channel}"
+    limitV = f";VOLT:LIM?"
+    return channel + limitV
 
-# def get_limitC(channel):
-#     channel = f"INST CH{channel}"
-#     limitC = f";CURR:LIM?"
-#     return channel + limitC
-    
+def get_limitC(channel):
+    channel = f"INST CH{channel}"
+    limitC = f";CURR:LIM?"
+    return channel + limitC
+
+# def get_voltage_ch1():
+#     channel = "1"
+#     message = get_voltage(channel)
+#     try:
+#         tcp_client.client_socket.send(message.encode())
+#     except:
+#         print("getch1: failed to send message")
+#     try:
+#         data = tcp_client.client_socket.recv(1024).decode()
+#         print(data)
+#     except:
+#         print("getch1: failed to recieve data")
